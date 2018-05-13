38c5ba98ea4d424c897cdea8487c66d6d01b17f7
Stephanie Metzner <stephaniemetzner@gmail.com>
2018-05-13 04:42:34 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,7 +15,7 @@ end
     if side1 == side2 && side2 == side3
       :equilateral
     elsif
-      side1 == side2 || side2 == side3| side1 == side3
+      side1 == side2 || side2 == side3|| side1 == side3
       :isoscles
     else
       :scalene
