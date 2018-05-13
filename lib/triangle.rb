750b74db8d3a259b7f42647fb38bacf7c5dca430
Stephanie Metzner <stephaniemetzner@gmail.com>
2018-05-13 04:04:11 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,18 +1,21 @@
 class Triangle
   
-  
+  class TriangleError < StandardError
+      # triangle error code
+    end
+
   attr_accessor :side1, :side2, :side3
- 
+
  def initialize(side1, side2, side3)
   @side1 = side1
   @side2 = side2
   @side3 = side3
 end
-  
+
   def kind(type)
     return type.to_sym
   end
- 
- 
+
+
 end
-end
+
